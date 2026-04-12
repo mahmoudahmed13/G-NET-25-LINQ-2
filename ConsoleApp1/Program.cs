17047fa08efd360aa2d8af77e689a3eb0135358c
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-04-12 23:06:03 +0200

    Question 01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ConsoleApp1
+﻿using static ConsoleApp1.DataSources.Source;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Question 01
+            ////1.Get top 3 most expensive products
             
+            //var result = ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+            #endregion
+
+            //foreach (var item in result)
+            //    Console.WriteLine(item);
         }
     }
 }
