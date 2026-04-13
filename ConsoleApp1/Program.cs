f8bed3c0a52eef2c58f7a13a5b9d68693541ba94
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-04-13 04:42:39 +0200

    Question 02

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,14 @@ namespace ConsoleApp1
         {
             #region Question 01
             ////1.Get top 3 most expensive products
-            
+
             //var result = ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+            #endregion
+
+            #region Question 02
+            //2.show page 2 of products, with page size = 5
+            int pageSize = 5, pageNumber = 2;
+            var result = ProductList.Skip((pageNumber -1 ) * pageSize).Take(pageSize);
             #endregion
 
             //foreach (var item in result)
